3d85b1156c54f719d77a4028f2015a5e073da484
Dafydd Vaughan <dafydd@alphagov.co.uk>
2011-12-21 12:12:49 +0000

    add Alan Maddrell's gravatar to list

--- application/helpers/publisher_user_helper.php
+++ application/helpers/publisher_user_helper.php
@@ -8,6 +8,7 @@ if ( ! function_exists('gravatar_hash')) {
   		case 'Simon Kaplan': return "008364c628b5df3ab16d14cec50e99ae"; break;
   		case 'Graham Spicer': return "ddf2e80bd5fc32be841441e92b5ec77d"; break;
   		case 'Julian Milne': return "cb50980366964966c1c03d0a30c2c155"; break;
+  		case 'Alan Maddrell': return "fa0afd560e18f96e7b605fb538315cc0"; break;
   		default: return "";
   	}
   	return "";
