fadc5a9011f80630dd699e99b1e4de36be1a2e15
Dafydd Vaughan <dafydd@alphagov.co.uk>
2012-01-21 23:13:47 +0000

    adding user helper to autoload - it's needed pretty much everywhere

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database','session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('publisher_user_helper');
 
 
 /*
