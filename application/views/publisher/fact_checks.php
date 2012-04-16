b1f106a9e2b22ea8d565842fc7335e1c402e0c40
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2012-04-16 10:22:24 +0100

    use new gravatar_hash field instead of function

--- application/views/publisher/fact_checks.php
+++ application/views/publisher/fact_checks.php
@@ -7,7 +7,7 @@
 <?php foreach($fact_checks as $msg) :?>
 <div class="fcitem">
   <div class="alert-message <?php echo time_based_class(mysql_to_unix($msg->action_date));?>">
-    <img src="http://www.gravatar.com/avatar/<?php if(gravatar_hash($msg->user)) : echo gravatar_hash($msg->user); else : echo "null"; endif; ?>?s=64" width="64" height="64" alt="#" />
+    <?php if($msg->gravatar_hash) :?><img src="http://www.gravatar.com/avatar/<?php echo $msg->gravatar_hash; ?>?s=64" width="64" height="64" alt="#" /><?php endif; ?>
     <strong><?php echo convert_to_human_date(mysql_to_unix($msg->action_date),2 ,'d-M-Y'); ?>: <em><?php echo $msg->user; ?></em></strong>
     <br/><?php echo $msg->title; ?> (<?php echo strtolower($msg->format); ?>)
     <br/>
--- application/views/publisher/publisher_updates.php
+++ application/views/publisher/publisher_updates.php
@@ -9,7 +9,7 @@
     <h3>Recently Published</h3>
     <?php foreach($published_messages as $msg) : ?>
       <div class="alert-message success">
-        <img src="http://www.gravatar.com/avatar/<?php if(gravatar_hash($msg->user)) : echo gravatar_hash($msg->user); else : echo "null"; endif; ?>?s=64" width="64" height="64" alt="#" />
+        <?php if($msg->gravatar_hash) :?><img src="http://www.gravatar.com/avatar/<?php echo $msg->gravatar_hash; ?>?s=64" width="64" height="64" alt="#" /><?php endif; ?>
         <strong><?php echo $msg->user; ?></strong> <?php echo $msg->action_name; ?> the <?php echo strtolower($msg->format); ?> <strong>"<?php echo $msg->title; ?>"</strong>
         <span><?php echo convert_to_human_date(mysql_to_unix($msg->action_date),2 ,'d-M-Y h:i:s'); ?></span>
       </div>
@@ -20,7 +20,7 @@
     <h3>Action Updates</h3>
     <?php foreach($action_messages as $msg) : ?>
       <div class="alert-message <?php echo $msg->action_format; ?>">
-        <?php if(gravatar_hash($msg->user)) : ?><img src="http://www.gravatar.com/avatar/<?php echo gravatar_hash($msg->user); ?>?s=32" width="32" height="32" alt="#" /><?php endif; ?>
+        <?php if($msg->gravatar_hash) :?><img src="http://www.gravatar.com/avatar/<?php echo $msg->gravatar_hash; ?>?s=32" width="32" height="32" alt="#" /><?php endif; ?>
         <?php echo $msg->action_name; ?>: <strong>"<?php echo $msg->title; ?>"</strong> (<?php echo strtolower($msg->format); ?>) by <strong><?php echo $msg->user; ?></strong>
         <span><?php echo convert_to_human_date(mysql_to_unix($msg->action_date),2 ,'d-M-Y h:i:s'); ?></span>
       </div>
